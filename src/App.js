979bae65a28bba8833c8b627aff6b977521e52f5
Saiful Ajom Khan <116379606+Sajomkhan@users.noreply.github.com>
2022-12-15 19:48:40 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import { useEffect, useState } from "react";
 
+import "./App.css";
+import MovieCard from "./MovieCard";
+import searchIcon from "./search.svg";
+
+
 //https://omdbapi.com/apikey.aspx?__EVENTTARGET=&__EVENTARGUMENT=&__LASTFOCUS=&__VIEWSTATE=%2FwEPDwUKLTIwNDY4MTIzNQ9kFgYCAQ9kFggCAQ8QDxYCHgdDaGVja2VkaGRkZGQCAw8QDxYCHwBnZGRkZAIFDxYCHgdWaXNpYmxlaGQCBw8WAh8BZ2QCAg8WAh8BaGQCAw8WAh8BaGQYAQUeX19Db250cm9sc1JlcXVpcmVQb3N0QmFja0tleV9fFgMFC3BhdHJlb25BY2N0BQtwYXRyZW9uQWNjdAUIZnJlZUFjY3SZmkfBgEVOtEhBRPgn0xJZZDjfMEiMoho3O8lIVPYLXg%3D%3D&__VIEWSTATEGENERATOR=5E550F58&__EVENTVALIDATION=%2FwEdAAhq8u7G6E8iNQTDLBqGZykXmSzhXfnlWWVdWIamVouVTzfZJuQDpLVS6HZFWq5fYphdL1XrNEjnC%2FKjNya%2Bmqh8hRPnM5dWgso2y7bj7kVNLSFbtYIt24Lw6ktxrd5Z67%2F4LFSTzFfbXTFN5VgQX9Nbzfg78Z8BXhXifTCAVkevd2U20ItIGqFIf8giu%2B0PAasvwu4KgXUo9rywyT%2ByOXGt&at=freeAcct&Email2=sajomkhan%40gmail.com&FirstName=Saiful&LastName=Ajom+Khan&TextArea1=for+practice+purpose&Button1=Submit
 
 // const movie1 = {
@@ -9,13 +14,6 @@ import { useEffect, useState } from "react";
 //   Type: "movie",
 //   Poster: "N/A",
 // };
-
-//6dbc3732
-
-import MovieCard from "./MovieCard";
-import "./App.css";
-import searchIcon from "./search.svg";
-
 
 const API_URL = "http://www.omdbapi.com?apikey=6dbc3732";
 
